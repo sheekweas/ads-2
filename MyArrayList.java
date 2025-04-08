72b2263ae8c40d315cae21b8f5c1c43b33e9e18b
Kurmash Shapagat <102404161+sheekweas@users.noreply.github.com>
2025-04-08 22:18:26 +0500

    comments in MyArrayList.java

--- MyArrayList.java
+++ MyArrayList.java
@@ -1,134 +1,153 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+// MyArrayList is a custom implementation of a dynamic array-based list
 public class MyArrayList<T extends Comparable<T>> implements MyList<T> {
-    private Object[] data;
-    private int size;
+    private Object[] elements; // Array to store elements
+    private int size; // The current number of elements
 
+    // Constructor initializes the array with an initial capacity
     public MyArrayList() {
-        data = new Object[10];
+        elements = new Object[10];
         size = 0;
     }
 
-    private void ensureCapacity() {
-        if (size == data.length) {
-            Object[] newData = new Object[data.length * 2];
-            for (int i = 0; i < size; i++) newData[i] = data[i];
-            data = newData;
-        }
-    }
-
+    @Override
     public void add(T item) {
         ensureCapacity();
-        data[size++] = item;
+        elements[size++] = item;
     }
 
+    @Override
     public void set(int index, T item) {
-        checkIndex(index);
-        data[index] = item;
+        if (index >= size || index < 0) throw new IndexOutOfBoundsException();
+        elements[index] = item;
     }
 
+    @Override
     public void add(int index, T item) {
-        if (index < 0 || index > size) throw new IndexOutOfBoundsException();
+        if (index > size || index < 0) throw new IndexOutOfBoundsException();
         ensureCapacity();
-        for (int i = size; i > index; i--) data[i] = data[i - 1];
-        data[index] = item;
+        for (int i = size; i > index; i--) {
+            elements[i] = elements[i - 1];
+        }
+        elements[index] = item;
         size++;
     }
 
+    @Override
     public void addFirst(T item) {
         add(0, item);
     }
 
+    @Override
     public void addLast(T item) {
         add(item);
     }
 
-    @SuppressWarnings("unchecked")
+    @Override
     public T get(int index) {
-        checkIndex(index);
-        return (T) data[index];
+        if (index >= size || index < 0) throw new IndexOutOfBoundsException();
+        return (T) elements[index];
     }
 
+    @Override
     public T getFirst() {
-        return get(0);
+        if (size == 0) throw new NoSuchElementException();
+        return (T) elements[0];
     }
 
+    @Override
     public T getLast() {
-        return get(size - 1);
+        if (size == 0) throw new NoSuchElementException();
+        return (T) elements[size - 1];
     }
 
+    @Override
     public void remove(int index) {
-        checkIndex(index);
-        for (int i = index; i < size - 1; i++) data[i] = data[i + 1];
-        data[--size] = null;
+        if (index >= size || index < 0) throw new IndexOutOfBoundsException();
+        for (int i = index; i < size - 1; i++) {
+            elements[i] = elements[i + 1];
+        }
+        elements[--size] = null;
     }
 
+    @Override
     public void removeFirst() {
         remove(0);
     }
 
+    @Override
     public void removeLast() {
-        remove(size - 1);
+        if (size == 0) throw new NoSuchElementException();
+        elements[--size] = null;
     }
 
-    @SuppressWarnings("unchecked")
+    @Override
     public void sort() {
-        for (int i = 0; i < size - 1; i++) {
-            int minIdx = i;
-            for (int j = i + 1; j < size; j++) {
-                if (((T) data[j]).compareTo((T) data[minIdx]) < 0) minIdx = j;
-            }
-            Object temp = data[i];
-            data[i] = data[minIdx];
-            data[minIdx] = temp;
-        }
+        Arrays.sort((T[]) elements, 0, size);
     }
 
+    @Override
     public int indexOf(Object object) {
-        for (int i = 0; i < size; i++) if (data[i].equals(object)) return i;
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(object)) return i;
+        }
         return -1;
     }
 
+    @Override
     public int lastIndexOf(Object object) {
-        for (int i = size - 1; i >= 0; i--) if (data[i].equals(object)) return i;
+        for (int i = size - 1; i >= 0; i--) {
+            if (elements[i].equals(object)) return i;
+        }
         return -1;
     }
 
+    @Override
     public boolean exists(Object object) {
         return indexOf(object) != -1;
     }
 
+    @Override
     public Object[] toArray() {
-        Object[] result = new Object[size];
-        for (int i = 0; i < size; i++) result[i] = data[i];
-        return result;
+        return Arrays.copyOf(elements, size);
     }
 
+    @Override
     public void clear() {
-        for (int i = 0; i < size; i++) data[i] = null;
+        Arrays.fill(elements, null);
         size = 0;
     }
 
+    @Override
     public int size() {
         return size;
     }
 
-    public Iterator<T> iterator() {
-        return new Iterator<T>() {
-            private int cursor = 0;
-            public boolean hasNext() {
-                return cursor < size;
-            }
-            @SuppressWarnings("unchecked")
-            public T next() {
-                if (!hasNext()) throw new NoSuchElementException();
-                return (T) data[cursor++];
-            }
-        };
+    // Ensures the array has enough capacity to accommodate new elements
+    private void ensureCapacity() {
+        if (size >= elements.length) {
+            elements = Arrays.copyOf(elements, elements.length * 2);
+        }
     }
 
-    private void checkIndex(int index) {
-        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+    // Iterator implementation for MyArrayList
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) throw new NoSuchElementException();
+                return (T) elements[index++];
+            }
+        };
     }
 }
