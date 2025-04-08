191eff094908a07b32ec3bc2836832ef5a007d06
Kurmash Shapagat <102404161+sheekweas@users.noreply.github.com>
2025-04-08 22:20:11 +0500

    comments in MyQueue.java

--- MyQueue.java
+++ MyQueue.java
@@ -1,19 +1,24 @@
+// MyQueue is a custom implementation of the queue data structure
 public class MyQueue<T extends Comparable<T>> {
-    private final MyLinkedList<T> list = new MyLinkedList<>();
+    private MyLinkedList<T> list;
+
+    public MyQueue() {
+        list = new MyLinkedList<>();
+    }
 
     public void enqueue(T item) {
         list.addLast(item);
     }
 
     public T dequeue() {
-        if (isEmpty()) throw new RuntimeException("Queue is empty");
+        if (isEmpty()) throw new NoSuchElementException();
         T item = list.getFirst();
         list.removeFirst();
         return item;
     }
 
     public T peek() {
-        if (isEmpty()) throw new RuntimeException("Queue is empty");
+        if (isEmpty()) throw new NoSuchElementException();
         return list.getFirst();
     }
 
